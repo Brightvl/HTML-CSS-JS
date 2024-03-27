8e0d90b21505a0560521458f830e831fdb228237
Iaroslav S <ziro-x-100@mail.ru>
2024-03-27 16:01:05 +0300

    L3 JS -> done

---
+++ basics_JavaScript/Lesson_3_function/L3/L3.js
@@ -0,0 +1,25 @@
+// const prompt = require("prompt-sync")({sigint:true}); // для того чтобы в js консоли работал prompt
+// // если функция ничего не возвращает
+// function helloName(name) {
+//     console.log(name);
+// }
+// helloName('alex');
+//
+//
+// let count = 5;
+// function counter() {
+//     return count++;
+// }
+// counter();
+// console.log(count)
+//
+// // если функция возвращает, то =>
+// let age = Number(prompt("сколько вам лет"));
+// const lvlUpAge = () => age + 5;
+// console.log(`Через 5 лет вам будет ${lvlUpAge()}`);
+
+
+function sayHello() {
+    alert("привет")
+    alert("нажали на кнопку")
+}
