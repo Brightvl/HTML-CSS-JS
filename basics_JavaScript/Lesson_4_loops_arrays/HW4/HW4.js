fade28c6e498fe29a53af0982d0eef0777e0ab8f
Iaroslav S <ziro-x-100@mail.ru>
2024-04-02 18:12:55 +0300

    HW5 JS-> in progress

--- basics_JavaScript/Lesson_4_loops_arrays/HW4/HW4.js
+++ basics_JavaScript/Lesson_4_loops_arrays/HW4/HW4.js
@@ -6,23 +6,14 @@
 // 3 – нечетное число
 // …
 // 10 – четное число
-console.log('Задание 1:');
-
-function sequence() {
-    console.log(0 + ' – это ноль');
-    for (let i = 1; i <= 10; i++) {
-        console.log((i % 2 === 0) ? (i + ' – четное число') : (i + ' – нечетное число'));
-    }
+for (let i = 0; i <= 11; i++) {
+    if (i === 0) console.log(`${i} - это ноль`);
+    else if (i % 2 === 0) console.log(`${i} - это четное число`);
+    else console.log(`${i} - это нечетное`);
 }
-
-sequence()
-
-
 // Задание 2
 // Дан массив [1, 2, 3, 4, 5, 6, 7]
 // Сделайте из этого массива следующий [1, 2, 3, 6, 7]
-// Задание 2 (без filter)
-console.log('Задание 2:');
 const array = [1, 2, 3, 4, 5, 6, 7];
 const newArray = array.slice(0, 3).concat(array.slice(-2));
 console.log(newArray);
@@ -33,33 +24,34 @@ console.log('стало ' + newArray);
 
 // Задание 3
 // Используя Math.random() вам необходимо генерировать цифры от 0 до 9, и создать массив состоящий из 5 таких элементов
-console.log('Задание 3:');
-const randomArray = (count) => {
-    let arr = []
-    for (let i = 0; i < count; i++) {
-        arr.push(Math.floor(Math.random() * 10));
+const getRandomArray = (number = 1) => {
+    let tempArr = []
+    for (let i = 0; i < number; i++) {
+        tempArr.push(Math.floor(Math.random() * 10));
     }
-    return arr;
+    return tempArr;
 }
+const randomArray = getRandomArray(5);
+console.log("Массив случайных чисел:", randomArray);
 
-const arr = randomArray(5)
-
-console.log('Массив:', arr);
 // 1. Рассчитать сумму элементов этого массива
-
-let getSum = (number = []) => {
-    let sum = 0;
-    for (let i = 0; i < number.length; i++) sum += number[i];
-    return sum;
-}
-console.log('Сумма элементов:', getSum(arr));
+const sumArray = (number = []) => {
+    let sum2 = 0;
+    for (const numberKey of number) {
+        sum2 += +numberKey;
+    }
+    return sum2
+};
+console.log("Сумма элементов массива:", sumArray(randomArray));
 
 // 2. Найти минимальное число
-const min = Math.min(...arr);
-console.log('Минимальное число:', min);
+const minNumber = Math.min(...randomArray);
+console.log("Минимальное число в массиве:", minNumber);
 
 // 3. Найти есть ли в этом массиве число 3
-console.log('Есть ли число 3:', arr.includes(3));
+const includesThree = randomArray.includes(3);
+console.log("Массив содержит число 3:", includesThree);
+
 
 // *Необязательное задание. *
 // Необходимо вывести горку в консоль (используя цикл for), как показано на рисунке, только у вашей горки должно быть 20 рядов, а не 5:
