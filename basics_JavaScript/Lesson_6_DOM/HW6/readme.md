5c9e0b65e162f2566e05c5b2ca94f84e42b153c2
Iaroslav S <ziro-x-100@mail.ru>
2024-04-04 12:32:01 +0300

    HW6 JS -> Done

---
+++ basics_JavaScript/Lesson_6_DOM/HW6/readme.md
@@ -0,0 +1,41 @@
+Выполнить все задачи в теге script. Комментарии, в которых написаны задачи, не
+стирать, код с решением задачи пишем под комментарием.
+
+```html
+<a href="#" class="card-link">Card link</a>
+<a href="#" class="card-link">Another link</a>
+
+<div class="card" style="width: 18rem;">
+<div class="card-body">
+<h5 class="card-title" data-number="100">Card title</h5>
+<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+<p class="card-text" data-number="50">
+Some quick example text to build on the card title and make up the bulk of the card's
+content.
+</p>
+<a href="#" id="super_link" class="card-link">Card link</a>
+<a href="#" class="card-link" data-number="50">Another link</a>
+</div>
+</div>
+
+<script>
+"use strict";
+```
+
+[]()
+[]()
+1. Найти по id, используя getElementById, элемент с id равным "super_link" и вывести этот элемент в консоль.
+[]()
+2. Внутри всех элементов на странице, которые имеют класс "card-link", поменяйте текст внутри элемента на "ссылка".
+[]()
+3. Найти все элементы на странице с классом "card-link", которые лежат в элементе с классом "card-body" и вывести полученную коллекцию в консоль.
+[]()
+4. Найти первый попавшийся элемент на странице у которого есть атрибут data-number со значением 50 и вывести его в консоль.
+[]()
+5. Выведите содержимое тега title в консоль.
+[]()
+6. Получите элемент с классом "card-title" и выведите его родительский узел в консоль.
+[]()
+7. Создайте тегp`, запишите внутри него текст "Привет" и добавьте созданный тег в начало элемента, который имеет класс "card".
+
+8. Удалите тег h6 на странице.
--- basics_JavaScript/Lesson_6_DOM/S6/S6.js
+++ basics_JavaScript/Lesson_6_DOM/S6/S6.js
@@ -1,13 +1,13 @@
-//Задание 1
-// получили ссылку
-// найти первого ребенка
-// const div = document.querySelector('#block p:first-child');
-// console.log(div);
+// Задание 1.1
+// Получите ссылку на первый абзац из дива с id,
+// равным block, выведите его в консоль
 
-//получили абзац
-// найти из всех по индексу
-// const div = document.querySelectorAll('p');
-// console.log(div[1]);
+const div11 = document.querySelector('#block p:first-child');
+console.log(div11);
+
+// Получите ссылку на первый абзац с классом www. и вывести его в консоль
+const div12 = document.querySelectorAll('p');
+console.log(div12[1]);
 
 // перебор всех параграфов и поиск внутри текста
 const div = document.querySelectorAll('p');
@@ -24,13 +24,73 @@ const link = document.querySelector('.link');
 const img = document.querySelector('.photo');
 
 // задание 2.1
+// Вам необходимо поменять текст внутри ссылки на “link text js”
 link.textContent = 'link text js';
-// link.href = 'http'; //1 вариант
-link.setAttribute('href', 'http') //2 вариант
+// Заменить href, на значение https://developer.mozilla.org/ru/
+// link.href = 'https://developer.mozilla.org/ru/'; //1 вариант
+link.setAttribute('href', 'https://developer.mozilla.org/ru/') //2 вариант
 
 // задание 2.2
+// Вам необходимо с помощью js поменять значение src на любое изображение из интернета
 // img.src = 'https://th.bing.com/th/id/R.d94321c9d76ba1810cd2e4883b5b2e20?rik=Yxo9TcLklYI6nA&pid=ImgRaw&r=0/'; //1 вариант
 img.width = 300;
 img.setAttribute('src', 'https://th.bing.com/th/id/R.d94321c9d76ba1810cd2e4883b5b2e20?rik=Yxo9TcLklYI6nA&pid=ImgRaw&r=0/'); //2 вариант
 
 // Задание 3
+// 1. Дан тег <div class="content"></div>
+// 2. Создайте новый элемент p
+// 3. Добавьте в него текст “Новый текстовый элемент”
+// 4. Добавьте созданный элемент внутри <div class="content"></div>
+// 5. Удалите добавленный узел
+const div2 = document.querySelector('.content');
+const newTxt = document.createElement('p');
+newTxt.innerHTML = '<span>Новый текстовой элемент</span>'; // inHTML когда нужно вписать теги HTML
+// newTxt.textContent = 'Новый текстовой элемент'; // есть смысл просто так писать
+div2.appendChild(newTxt);
+newTxt.remove();
+
+// Задание 4
+// 1. Создать элемент button, добавить в блок <div class="content"></div>
+const but = document.querySelector('.button');
+div2.appendChild(but);
+
+let count = 0;
+
+but.addEventListener('click', () => {
+    count++;
+    console.log(`Количество нажатий ${count}`)
+})
+
+// 2. При клике на который в консоль выводится сколько раз
+// пользователь нажал на данную кнопку
+const newBtn = document.createElement('button');
+newBtn.textContent = 'Обнулить';
+div2.appendChild(newBtn);
+newBtn.onclick = clickReset;
+
+function clickReset() {
+    count = 0;
+    console.log(`Счетчик обнулен`)
+}
+
+// Задание 5
+// 1. Дан тег <div class="content"></div>
+// 2. Создайте с помощью javascript новый элемент button
+// 3. Добавьте текст для кнопки “Отправить”
+// 4. При клике на данную кнопку необходимо чтобы текст
+// поменялся на “Текст отправлен”
+
+const textBtn = document.createElement('button');
+textBtn.textContent = 'Отправить';
+div2.appendChild(textBtn);
+textBtn.addEventListener('click', () => {
+
+    setTimeout(
+        () => {
+            textBtn.textContent = 'Текст отправлен';
+        },
+        1000);
+});
+
+
+
