c56c0c2d48ee71e81fabe552750387b1c7b6f1f5
Iaroslav S <ziro-x-100@mail.ru>
2024-04-16 11:56:05 +0300

    hw3 JS ecma-> done

--- JavaScript/3_ECMAScript/Lesson_3_OOP2/HW3/hw3.js
+++ JavaScript/3_ECMAScript/Lesson_3_OOP2/HW3/hw3.js
@@ -1,15 +1,29 @@
-
 // Задание 1: ""Управление персоналом компании""
-//
 // Реализуйте класс Employee (сотрудник), который имеет следующие свойства и методы:
-//
-//     Свойство name (имя) - строка, имя сотрудника.
-//     Метод displayInfo() - выводит информацию о сотруднике (имя).
+class Employee {
+    // Свойство name (имя) - строка, имя сотрудника.
+    constructor(name) {
+        this.name = name;
+    }
+    // Метод displayInfo() - выводит информацию о сотруднике (имя).
+    displayInfo() {
+        console.log(`Name: ${this.name}`);
+    }
+}
 //     Реализуйте класс Manager (менеджер), который наследует класс Employee и имеет дополнительное свойство и метод:
-//
-//     Свойство department (отдел) - строка, отдел, в котором работает менеджер.
-//     Метод displayInfo() - переопределяет метод displayInfo() родительского класса и выводит информацию о менеджере (имя и отдел).
-// Пример использования классов
+class Manager extends Employee {
+    // Свойство department (отдел) - строка, отдел, в котором работает менеджер.
+    constructor(name, department) {
+        super(name);
+        this.department = department;
+    }
+    // Метод displayInfo() - переопределяет метод displayInfo() родительского класса и выводит информацию о менеджере (имя и отдел).
+    displayInfo() {
+        console.log(`Name: ${this.name} Department: ${this.department}`);
+    }
+}
+
+
 const employee = new Employee("John Smith");
 employee.displayInfo();
 // Вывод:
@@ -21,17 +35,34 @@ manager.displayInfo();
 // Name: Jane Doe
 // Department: Sales
 
-"Управление списком заказов"
+// "Управление списком заказов"
 
 // Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
-//
-//     Свойство orderNumber (номер заказа) - число, уникальный номер заказа.
-//     Свойство products (продукты) - массив, содержащий список продуктов в заказе.
-//     Метод addProduct(product) - принимает объект product и добавляет его в список продуктов заказа.
-//     Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+class Order {
+    // Свойство orderNumber (номер заказа) - число, уникальный номер заказа.
+    // Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+    constructor(orderNumber) {
+        this.orderNumber = orderNumber;
+        this.products = [];
+    }
+
+    // Метод addProduct(product) - принимает объект product и добавляет его в список продуктов заказа.
+    addProduct(product) {
+        this.products.push(product);
+    }
+
+    // Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+    getTotalPrice() {
+        let totalPrice = 0;
+        for (let product of this.products) {
+            totalPrice += product.price;
+        }
+        return totalPrice;
+    }
+}
 
 // Пример использования класса
-    class Product {
+class Product {
     constructor(name, price) {
         this.name = name;
         this.price = price;
@@ -39,11 +70,8 @@ manager.displayInfo();
 }
 
 const order = new Order(12345);
-
 const product1 = new Product("Phone", 500);
 order.addProduct(product1);
-
 const product2 = new Product("Headphones", 100);
 order.addProduct(product2);
-
 console.log(order.getTotalPrice()); // Вывод: 600
